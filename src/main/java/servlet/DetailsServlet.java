9e7ad50907680f3934fa8fe43309960206602c86
Чумаченко Артём Александрович <AAChumachenko@cbgr.ru>
2023-03-26 16:14:37 +0300

    При нажатии на кнопку подробнее под названиями отелей, клиент отправляет на сервер buttonId нажатой кнопки

---
+++ src/main/java/servlet/DetailsServlet.java
@@ -0,0 +1,25 @@
+package servlet;
+
+import generator.HotelPageGenerator;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "DetailsServlet", urlPatterns = {"/details"})
+public class DetailsServlet extends HttpServlet {
+    HotelPageGenerator hotelPageGenerator = new HotelPageGenerator();
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String buttonId = request.getParameter("buttonId");
+        System.out.println(buttonId);
+        // здесь можно использовать buttonId для определения какая кнопка была нажата
+        // и получить соответствующую информацию об отеле
+        // Отправляем ответ
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().print(hotelPageGenerator.getHotelDetails());
+    }
+}
